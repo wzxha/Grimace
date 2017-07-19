39d5d330e554b338607b73bb89a2ff098d41ea1f
wzxjiang <wzxjiang@foxmail.com>
2017-07-19 18:32:41 +0800

    + [Basic] Init

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by wzxjiang on 2017/7/19.
+//  Copyright © 2017年 wzxjiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
