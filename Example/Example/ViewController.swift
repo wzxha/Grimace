3eceb3efa167943ba1752303dfac3f9d061e9d96
wzxjiang <wzxjiang@foxmail.com>
2017-07-20 10:06:57 +0800

    + [Example] video capture

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import GPUImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet var outputView: GPUImageView?
+    
+    var videoCamera: GPUImageVideoCamera = GPUImageVideoCamera.init(sessionPreset: AVCaptureSessionPreset1280x720, cameraPosition: .front)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupVideoCamera()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupVideoCamera() {
+        videoCamera.addAudioInputsAndOutputs()
+        
+        videoCamera.addTarget(outputView)
+        
+        videoCamera.outputImageOrientation = .portrait
+        
+        videoCamera.horizontallyMirrorRearFacingCamera = false
+        videoCamera.horizontallyMirrorFrontFacingCamera = true
+        
+        videoCamera.delegate = self
+        
+        videoCamera.startCapture()
     }
+    
+}
 
-
+extension ViewController: GPUImageVideoCameraDelegate {
+    func willOutputSampleBuffer(_ sampleBuffer: CMSampleBuffer!) {
+        
+    }
 }
 
